223e8db3cbaf53c9d45109f332a10109629263b6
Ryan Snoyman <rsnoyman@atlassian.com>
2023-01-26 23:34:56 +1100

    exercise 4

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -1,11 +1,16 @@
 import React from "react";
 import { range } from "../../utils";
+import { checkGuess } from "../../game-helpers";
 
-function Guess({ guess }) {
+function Guess({ guess, answer }) {
+  const check = checkGuess(guess, answer);
   return (
     <p className="guess">
       {range(5).map((letter) => (
-        <span key={`letter-${letter}`} className="cell">
+        <span
+          key={`letter-${letter}`}
+          className={`cell ${check && check[letter].status}`}
+        >
           {guess && guess[letter]}
         </span>
       ))}
